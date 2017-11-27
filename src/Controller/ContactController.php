2d15cb1cd54a3c4724d6d7457d5cf566758fe5aa
Hetphil <p_bordmann@orange.fr>
2017-11-27 10:31:11 +0100

    correction on submitting form add a flashBag

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,9 +2,9 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use App\Form\ContactType;
 use App\Event\ContactEvent;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use App\Entity\Contact;
 
 class ContactController extends Controller
@@ -28,11 +28,14 @@ class ContactController extends Controller
             $em->persist($contact);
             $em->flush();
 
-            $this->redirectToRoute('home');
+            $session = $this->container->get('session');
+            $session->getFlashBag()->set('success', 'You\'re message has been send.');
+
+            $this->redirectToRoute('contact');
         }
 
         return $this->render('contactForm.html.twig', array(
-           'form' => $form->createView()
+            'form' => $form->createView()
         ));
     }
 }
