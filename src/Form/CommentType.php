764cf3b01845670cd37d8f5c40e32932672e7099
Hetphil <p_bordmann@orange.fr>
2017-12-27 12:54:48 +0100

    - create Entity : Comment - create Form : CommentType

---
+++ src/Form/CommentType.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Form;
+
+use Gregwar\CaptchaBundle\Type\CaptchaType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class CommentType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('author', TextType::class, array(
+                'attr' => array(
+                    'placeholder' => 'Name'
+                )
+            ))
+            ->add('message', TextareaType::class, array(
+                'label' => 'Commentaire',
+                'attr' => array(
+                    'placeholder' => 'Your Comment',
+                    'style' => 'height: 75px'
+                )
+            ))
+            ->add('captcha', CaptchaType::class, array(
+                'label' => 'Captcha',
+                'reload' => true,
+                'as_url' => true
+            ))
+            ->add('submit', SubmitType::class, array(
+                'attr' => array(
+                    'class' => 'btn-primary pull-left'
+                ),
+                'label' => 'Send'
+            ))
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'App\Entity\Comment'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'comment';
+    }
+}
