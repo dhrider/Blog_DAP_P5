02dce97556dbdb53cd979ead3bea68099db6b937
Hetphil <p_bordmann@orange.fr>
2018-01-02 14:17:13 +0100

    - create Entity : User - create Repository : UserRepository - create Admin PostController - create SecurityController - modify security.yml - add routes : login, login_check & logout - create view : login - modify view 'base' : add admin link in footer

---
+++ src/Controller/Admin/PostController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class PostController extends Controller
+{
+
+}
---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SecurityController extends Controller
+{
+    public function loginAction()
+    {
+        $helper = $this->get('security.authentication_utils');
+
+        return $this->render(':Security:login.html.twig', [
+           'last_username' => $helper->getLastUsername(),
+            'error' => $helper->getLastAuthenticationError()
+        ]);
+    }
+
+    public function loginCheckAction()
+    {
+
+    }
+
+    public function logoutAction()
+    {
+
+    }
+}
