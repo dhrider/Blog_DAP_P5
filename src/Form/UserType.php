63df14b7a9fe16ac86dde5f67fea55d4aef6a3dd
Hetphil <p_bordmann@orange.fr>
2018-01-03 16:34:43 +0100

    - create function : registerUser

---
+++ src/Form/UserType.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Gregwar\CaptchaBundle\Type\CaptchaType;
+
+class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class, array(
+                'label' => false,
+                'attr' => array(
+                    'placeholder' => 'Choose a username'
+                )
+            ))
+            ->add('password',RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password are not the same !',
+                'first_options' => array(
+                    'label' => false,
+                    'attr' => array(
+                        'placeholder' => 'Choose a Password'
+                    )
+                ),
+                'second_options' => array(
+                    'label' => false,
+                    'attr' => array(
+                        'placeholder' => 'Repeat your Password'
+                    )
+                )
+            ))
+            ->add('email', RepeatedType::class, array(
+                'type' => EmailType::class,
+                'invalid_message' => 'The emails are not the same !',
+                'first_options' => array(
+                    'label' => false,
+                    'attr' => array(
+                        'placeholder' => 'Your email'
+                    )
+                ),
+                'second_options' => array(
+                    'label' => false,
+                    'attr' => array(
+                        'placeholder' => 'Repeat you email'
+                    )
+                )
+            ))
+            ->add('captcha', CaptchaType::class, array(
+                'label' => false,
+                'reload' => true,
+                'as_url' => true,
+                'attr' => array(
+                    'placeholder' => 'Enter the same text as above'
+                )
+            ))
+            ->add('submit', SubmitType::class, array(
+                'attr' => array(
+                    'class' => 'btn-primary pull-left'
+                ),
+                'label' => 'Send'
+            ))
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'App\Entity\User'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'user';
+    }
+}
