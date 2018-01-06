857c6526c82e28f1df444d2b38c634a7c4dc3b49
Hetphil <p_bordmann@orange.fr>
2018-01-06 13:28:29 +0100

    - validation of the new password

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -17,6 +17,8 @@ class UserController extends Controller
 
         $form = $this->createForm(UserType::class, $user);
 
+        $session = $this->container->get('session');
+
         if ($request->isMethod('POST') && $form->handleRequest($request)->isValid())
         {
             $hash = $this
@@ -35,7 +37,6 @@ class UserController extends Controller
 
             $success = true;
 
-            $session = $this->container->get('session');
             $session
                 ->getFlashBag()
                 ->set('success', 'You\'ve been successfully registred. 
@@ -60,6 +61,8 @@ class UserController extends Controller
 
             $user = $em->getRepository(User::class)->findByEmail($request->request->get('email'));
 
+            $session = $this->container->get('session');
+
             if ($user)
             {
                 $date = new \DateTime();
@@ -79,12 +82,10 @@ class UserController extends Controller
 
                 $mailer->send($email);
 
-                $session = $this->container->get('session');
                 $session->getFlashBag()->set('success', 'Your username has been successfully send to your email.');
             }
             else
             {
-                $session = $this->container->get('session');
                 $session->getFlashBag()->set('warning', 'This email doesn\'t exist.');
             }
         }
@@ -100,8 +101,15 @@ class UserController extends Controller
 
             $user = $em->getRepository(User::class)->findByEmail($request->request->get('email'));
 
+            $session = $this->container->get('session');
+
             if ($user)
             {
+                $user->setToken(hash("sha512", uniqid()));
+
+                $em->persist($user);
+                $em->flush();
+
                 $email = new \Swift_Message;
 
                 $email
@@ -117,7 +125,6 @@ class UserController extends Controller
 
                 $mailer->send($email);
 
-                $session = $this->container->get('session');
                 $session->getFlashBag()->set('success', 'A link for resetting your password has been send to your email.');
             }
             else
@@ -130,11 +137,45 @@ class UserController extends Controller
         return $this->render(':User:resetPasswordUser.html.twig');
     }
 
-    public function newPasswordUserAction(User $user)
+    public function newPasswordUserAction(User $user, Request $request)
     {
         $em = $this->getDoctrine()->getManager();
 
         $userExist = $em->getRepository(User::class)->findOneBy(array("token" => $user->getToken()));
+
+        $session = $this->container->get('session');
+
+        if ($request->isMethod('POST'))
+        {
+            if ($request->request->get('password1') !== $request->request->get('password2'))
+            {
+                $session->getFlashBag()->set('warning', 'The password are not the same.');
+
+                return $this->redirectToRoute('new_password_user', array(
+                    'token' => $user->getToken()
+                ));
+            }
+            else
+            {
+                $hash = $this
+                    ->get('security.encoder_factory')
+                    ->getEncoder($user)
+                    ->encodePassword($request->request->get('password1'), $user->getSalt())
+                ;
+
+                $user->setPassword($hash);
+
+                $em->persist($user);
+                $em->flush();
+
+                $success = true;
+                $session->getFlashBag()->set('success', 'Your new password is now valid.');
+
+                return $this->redirectToRoute('login', array(
+                    'success' => $success
+                ));
+            }
+        }
 
         if ($userExist)
         {
@@ -142,7 +183,9 @@ class UserController extends Controller
         }
         else
         {
-            return $this->redirectToRoute('home');
+            $session->getFlashBag()->set('warning', 'The link for resetting your password is not valid.');
+
+            return $this->redirectToRoute('login');
         }
     }
 }
